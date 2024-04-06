fb6aa7f00db5f1692afbe135e7700fc323e4a28e
mkummeler <manon.kummeler@student.uclouvain.be>
2024-04-06 12:12:35 +0200

    .

--- src/code.h
+++ src/code.h
@@ -4,6 +4,9 @@
 
 Point* calculate_arc(double radius, double start_angle, double end_angle, int n_points);
 
+void addPoint (Point *points, int *nPoints, double x, double y)
+void addLine (Line *lines, int *nLines, Point *points, int point1, int point2) 
+
 Mesh* createMesh(const char *filename);
 void printMesh(Mesh *mesh);
 void freeMesh(Mesh *mesh);
