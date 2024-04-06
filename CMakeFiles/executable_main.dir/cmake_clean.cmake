95c67b8298adafec471c92d98532d77955460058
mkummeler <manon.kummeler@student.uclouvain.be>
2024-04-06 14:04:10 +0200

    premier commit

---
+++ CMakeFiles/executable_main.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/executable_main.dir/src/main.c.o"
+  "CMakeFiles/executable_main.dir/src/main.c.o.d"
+  "executable_main"
+  "executable_main.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/executable_main.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
